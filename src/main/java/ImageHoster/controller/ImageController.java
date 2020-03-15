50abc6315773f9a169ab88e47b6ff4b0b9bcceef
Kabir <kabir4691@gmail.com>
2020-03-15 08:32:02 +0530

    Fix url redirect path after image edit

--- src/main/java/ImageHoster/controller/ImageController.java
+++ src/main/java/ImageHoster/controller/ImageController.java
@@ -132,7 +132,7 @@ public class ImageController {
         updatedImage.setDate(new Date());
 
         imageService.updateImage(updatedImage);
-        return "redirect:/images/" + updatedImage.getTitle();
+        return "redirect:/images/" + updatedImage.getId() + "/" + updatedImage.getTitle();
     }
 
 
